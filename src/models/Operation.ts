1b8cd5e0e9e5237ed9915b51d2fbceb70d0cb35a
Assaf Morami <assaf.morami@gmail.com>
2020-12-15 17:26:08 +0200

    update operations schema

--- src/models/Operation.ts
+++ src/models/Operation.ts
@@ -1,29 +1,38 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import mongoose, {Schema, Types} from "mongoose";
-import {SwapDocument, Swap} from "./Swap";
-
+import mongoose, { Schema, Types } from "mongoose";
+import { SwapDocument, Swap } from "./Swap";
 
 export interface OperationDocument extends mongoose.Document {
-    id: string;
-    status: number;
-    transactionHash?: string;
-    swap?: SwapDocument["_id"]; //string | typeof Types.ObjectId;
+  id: string;
+  status: number;
+  transactionHash?: string;
+  ethAddress?: string;
+  secretAddress?: string;
+  asset?: string;
+  amount?: string;
+  swap?: SwapDocument["_id"]; //string | typeof Types.ObjectId;
 }
 
-
 export const operationSchema = new Schema({
-    id: String,
-    status: {
-        type: Number,
-        // get: (value: number) => SwapStatus[value],
-        // set: (status: SwapStatus) => status.valueOf(),
-    },
-    swap: {
-        required: false,
-        type: Schema.Types.ObjectId,
-        ref: "swap"},
-    transactionHash: String,
+  id: String,
+  status: {
+    type: Number,
+    // get: (value: number) => SwapStatus[value],
+    // set: (status: SwapStatus) => status.valueOf(),
+  },
+  swap: {
+    required: false,
+    type: Schema.Types.ObjectId,
+    ref: "swap",
+  },
+  transactionHash: String,
+  ethAddress: { type: String, required: false },
+  secretAddress: { type: String, required: false },
+  asset: { type: String, required: false },
+  amount: { type: String, required: false },
 });
 
-
-export const Operation = mongoose.model<OperationDocument>("operation", operationSchema);
+export const Operation = mongoose.model<OperationDocument>(
+  "operation",
+  operationSchema
+);
