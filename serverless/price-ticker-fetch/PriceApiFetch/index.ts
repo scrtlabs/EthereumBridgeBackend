4f6f50fc2ebb0d0ecf7a1d1f799918c5ee79df58
Itzik Grossman <itzik@keytango.io>
2021-01-25 12:13:58 +0200

    SSCRT price oracle

--- serverless/price-ticker-fetch/PriceApiFetch/index.ts
+++ serverless/price-ticker-fetch/PriceApiFetch/index.ts
@@ -58,6 +58,7 @@ class CoinGeckoOracle implements PriceOracle {
     symbolMap = {
         "BTC": "bitcoin",
         "SCRT": "secret",
+        "SSCRT": "secret",
         "ETH": "ethereum",
         "OCEAN": "ocean-protocol",
         "USDT": "tether",
