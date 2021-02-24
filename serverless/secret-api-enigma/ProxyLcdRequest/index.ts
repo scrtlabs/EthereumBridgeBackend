96edcaeb10384bbc10476060743a7885745430f4
Cashmaney <itzik@enigma.co>
2021-02-24 14:45:09 +0200

    fix proxy for POST

--- serverless/secret-api-enigma/ProxyLcdRequest/index.ts
+++ serverless/secret-api-enigma/ProxyLcdRequest/index.ts
@@ -1,22 +1,39 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import fetch from "node-fetch";
-import { Agent } from 'https';
+import { Agent } from "https";
 
 const httpsAgent = new Agent({
     rejectUnauthorized: false,
+    minVersion: "TLSv1"
 });
 
-const forwardURL = `${process.env["forwardURL"]}`
+const forwardURL = `${process.env["forwardURL"]}`;
+
+const isEmptyObject = (obj: Record<string, any>) => {
+    for(const i in obj) {
+        return false;
+    }
+    return true;
+};
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    let route = context.bindingData.route;
+    const route = context.bindingData.route;
 
-    const url = req.query ? `${forwardURL}/${route}?` + new URLSearchParams(req.query) : `${forwardURL}/${route}`;
+    // if (req.method === "POST") {
+    //     context.log(req.query);
+    // }
+
+    const url = isEmptyObject(req.query) ? `${forwardURL}/${route}` : `${forwardURL}/${route}?` + new URLSearchParams(req.query);
+
+    // if (req.method === "POST") {
+    //     context.log(url);
+    //     context.log(req.body);
+    // }
 
     const response = await fetch(url, {
         method: req.method,
         headers: req.headers,
-        body: req.body,
+        body: JSON.stringify(req.body),
         agent: httpsAgent
     });
 
@@ -25,16 +42,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.res = {
         status: response.status,
         body: respBody
-    }
+    };
 
-    // if (response.ok) {
-    //     context.res = {
-    //         status: response.status,
-    //         body: respBody
-    //     }
-    // } else {
-    //
-    // }
 };
 
 export default httpTrigger;
