1504141d753934c1613ed80db57a3dfbc9975982
Itzik Grossman <itzik@keytango.io>
2021-03-21 22:37:31 +0200

    secret pairs

--- src/app.ts
+++ src/app.ts
@@ -88,6 +88,8 @@ app.use(lusca.xssProtection(true));
 app.get("/tokens/", tokenController.getTokenPairings);
 app.get("/tokens/:token", tokenController.getToken);
 
+app.get("/secret_tokens/", tokenController.getSecretTokens);
+
 app.get("/swaps/", swapController.getAllSwaps);
 app.get("/swaps/:swap", swapController.getSwapInfo);
 
@@ -102,7 +104,8 @@ app.get("/secretswap_pairs/", secretSwapPairsController.getSecretSwapPairs);
 
 app.get("/signer_health/", signerHealthController.getSignerHealth);
 
-app.get("/sushi_pool", secretSwapPairsController.getSushiPool);
+//app.get("/sushi_pool", secretSwapPairsController.getSushiPool);
+
 
 
 export default app;
---
+++ src/models/Tokens.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import mongoose from "mongoose";
+
+type TOKEN_USAGE = "BRIDGE" | "REWARDS" | "LPSTAKING";
+
+
+export interface TokenDocument extends mongoose.Document {
+    name: string;
+    address: string;
+    decimals: number;
+    price: string;
+    usage: TOKEN_USAGE[];
+    id: string;
+    hidden: boolean;
+    display_props: object;
+}
+
+
+export const tokenSchema = new mongoose.Schema({
+    name: String,
+    address: String,
+    decimals: {
+        type: Number,
+        default: 6,
+    },
+    id: String,
+    price: String,
+    symbol: String,
+    usage: Array,
+    hidden: Boolean,
+}, { collection: "secret_tokens" });
+
+// userSchema.pre("save", function save(next) {
+//     const user = this as UserDocument;
+//     if (user.isModified("uid")) { return next(); }
+//     user.uid = generateApiKey();
+//     return next();
+// });
+
+export const Tokens = mongoose.model<TokenDocument>("secret_tokens", tokenSchema);
