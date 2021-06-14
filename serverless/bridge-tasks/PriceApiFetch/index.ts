883385dbdf9325911aa7d0f086750d172c6072e8
Itzik Grossman <itzik@keytango.io>
2021-06-14 18:45:29 +0300

    Fixed XVS price

--- serverless/bridge-tasks/PriceApiFetch/index.ts
+++ serverless/bridge-tasks/PriceApiFetch/index.ts
@@ -125,7 +125,7 @@ class CoinGeckoOracle implements PriceOracle {
         "TRX": "tron",
         "CAKE": "pancakeswap-token",
         "BAKE": "bakerytoken",
-        "VNX": "venus",
+        "XVS": "venus",
         "LINA": "linear",
         "FINE": "refinable",
         "BUNNY": "pancake-bunny",
