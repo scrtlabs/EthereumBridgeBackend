b9e81af2b0f8a220b24b39a264493b1d33682abb
Cashmaney <itzik@enigma.co>
2021-07-14 15:51:52 +0300

    merged changes from testnet

--- scripts/db_migration.sh
+++ scripts/db_migration.sh
@@ -11,14 +11,14 @@ DST_DB_SWAP="$6"
 #	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DST_DB_BRIDGE --db $DST_DB_BRIDGE --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
 #done
 
-for COLLECTION in operations token_pairing
-do
-	mongodump --uri mongodb+srv://$USERNAME:$PASSWORD@cluster0.dka2m.mongodb.net/$SRC_DB --collection $COLLECTION --out $COLLECTION-bsc.out --forceTableScan
-	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DDST_DB_UI --db $DST_DB_UI --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
-done
-#
-#for COLLECTION in secret_tokens airdrop_merkle airdrop_merkle_secret cashback_stats rewards_data secret_tokens secret_votes secretswap_pairs secretswap_pools
+#for COLLECTION in operations token_pairing
 #do
 #	mongodump --uri mongodb+srv://$USERNAME:$PASSWORD@cluster0.dka2m.mongodb.net/$SRC_DB --collection $COLLECTION --out $COLLECTION-bsc.out --forceTableScan
-#	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DST_DB_SWAP --db $DST_DB_SWAP --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
+#	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DDST_DB_UI --db $DST_DB_UI --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
 #done
+#
+for COLLECTION in token_pairing # secret_tokens airdrop_merkle airdrop_merkle_secret cashback_stats rewards_data secret_tokens secret_votes secretswap_pairs secretswap_pools
+do
+	mongodump --uri mongodb+srv://$USERNAME:$PASSWORD@cluster0.dka2m.mongodb.net/$SRC_DB --collection $COLLECTION --out $COLLECTION-bsc.out --forceTableScan
+	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DST_DB_SWAP --db $DST_DB_SWAP --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
+done
