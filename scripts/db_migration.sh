9a40d40e15efa85ae0158415e4b06c6c20dbb6c5
Cashmaney <itzik@enigma.co>
2021-07-18 11:36:32 +0300

    moved ci to testnet-new

--- scripts/db_migration.sh
+++ scripts/db_migration.sh
@@ -17,7 +17,7 @@ DST_DB_SWAP="$6"
 #	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DDST_DB_UI --db $DST_DB_UI --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
 #done
 #
-for COLLECTION in token_pairing # secret_tokens airdrop_merkle airdrop_merkle_secret cashback_stats rewards_data secret_tokens secret_votes secretswap_pairs secretswap_pools
+for COLLECTION in secret_votes # token_pairing # secret_tokens airdrop_merkle airdrop_merkle_secret cashback_stats rewards_data secret_tokens  secretswap_pairs secretswap_pools
 do
 	mongodump --uri mongodb+srv://$USERNAME:$PASSWORD@cluster0.dka2m.mongodb.net/$SRC_DB --collection $COLLECTION --out $COLLECTION-bsc.out --forceTableScan
 	mongorestore --uri mongodb+srv://$USERNAME:$PASSWORD@bridge-testnet.ekhng.mongodb.net/$DST_DB_SWAP --db $DST_DB_SWAP --collection $COLLECTION $COLLECTION-bsc.out/$SRC_DB/$COLLECTION.bson
