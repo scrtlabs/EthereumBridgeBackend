a5e767a67b6922cd8b62ebfcfefa6986b30b5d46
Cashmaney <itzik@enigma.co>
2022-02-01 18:00:41 +0200

    binance proxy restore

--- serverless/api_proxies/secret-api-datahub/ProxyLcdRequest/index.ts
+++ serverless/api_proxies/secret-api-datahub/ProxyLcdRequest/index.ts
@@ -1,16 +1,18 @@
 import {AzureFunction, Context, HttpRequest, HttpRequestHeaders} from "@azure/functions";
-import fetch from "node-fetch";
+const fetch = require('node-fetch');
 
 const apiKey = process.env["dataHubApiKey"];
 const forwardURL = process.env["forwardURL"];
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    let route = context.bindingData.route;
+    let route = context.bindingData.route || '';
 
     if (!isEmptyObject(req.query)) {
         delete req.query["code"]
     }
-    route = route.split('/').slice(1).join('/');
+    // context.log(`before split ${JSON.stringify(route)}`)
+    // route = route.split('/').slice(1).join('/');
+    // context.log(`after split ${JSON.stringify(route)}`)
 
     let url = isEmptyObject(req.query) ? `${forwardURL}/${route}` : `${forwardURL}/${route}?` + new URLSearchParams(req.query);
 
